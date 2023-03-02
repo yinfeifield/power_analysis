de4dc33561789da5a9ad3c8d608ec68607155d42
Maximilian Benkert <90043913+Maximilian-Benkert@users.noreply.github.com>
2023-03-02 08:59:24 +0100

    Update 0_prep.R

--- 0_prep.R
+++ 0_prep.R
@@ -13,7 +13,7 @@ setwd("C:/Users/gglbs/OneDrive - Bayer/MyDocs/STAT/PowerAnalysis/Rfiles")
 
 connection <- DBI::dbConnect(odbc::odbc(), driver = "SQL Anywhere 17",
                              host = "by0vpb.bayer.cnb", database = "scout2w2",
-                             port = 2638, uid = "tsaapp", pwd = "tsa2019")
+                             port = 2638, uid = "", pwd = "")
 
 #.libPaths()
 
